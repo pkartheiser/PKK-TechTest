f280a2b3af12f8cd5e52015bb22362b40a43b8e0
pkartheiser86 <pkartheiser@gmail.com>
2019-04-03 16:56:03 -0600

    Prints size of bucket

---
+++ boto3filesize.py
@@ -0,0 +1,17 @@
+import boto3
+
+from AWSSecrets import ACCESS_KEY, SECRET_KEY
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+)
+bucket = s3.Bucket('pk-kapost-tech1')
+size = 0
+
+for o in bucket.objects.all():
+    if 'pk-kapost-tech1' in o.key:
+        print (o), o.size
+        size += o.size
+print ('s3 size = %.3f MB' % (size/1024/1024))
