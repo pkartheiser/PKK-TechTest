ee4d073027db257570f72875f6ffbaadb708f06b
pkartheiser86 <pkartheiser@gmail.com>
2019-04-02 19:07:51 -0600

    added spaces between = in boto3cred test awssecret and awskeyfeed

--- AWSKeyFeed.py
+++ AWSKeyFeed.py
@@ -4,6 +4,6 @@ from AWSSecrets import ACCESS_KEY, SECRET_KEY
 
 client = boto3.client(
     's3'
-    aws_access_key_id=ACCESS_KEY
-    aws_secret_access_key=SECRET_KEY
+    aws_access_key_id = ACCESS_KEY
+    aws_secret_access_key = SECRET_KEY
 )
