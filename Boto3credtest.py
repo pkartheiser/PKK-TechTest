e44d83f7a57193678f431f2eed3e5840d62e5c79
pkartheiser86 <pkartheiser@gmail.com>
2019-04-02 18:50:54 -0600

    Testing if boto3 accepts credentials AWSKeyFeed.py and AWSSecrets.py

---
+++ Boto3credtest.py
@@ -0,0 +1,12 @@
+import boto3
+
+from AWSSecrets import ACCESS_KEY, SECRET_KEY
+
+client = boto3.client(
+    's3'
+    aws_access_key_id=ACCESS_KEY
+    aws_secret_access_key=SECRET_KEY
+)
+
+for bucket in s3.buckets.all() :
+...     print(bucket.name)
