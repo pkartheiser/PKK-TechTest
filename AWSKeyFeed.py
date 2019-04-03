04e91100e2b8c89b8d5d34e2451fbbfdb7207004
pkartheiser86 <pkartheiser@gmail.com>
2019-04-02 19:25:50 -0600

    Copied from AWS boto3 documentation

--- AWSKeyFeed.py
+++ AWSKeyFeed.py
@@ -3,7 +3,7 @@ import boto3
 from AWSSecrets import ACCESS_KEY, SECRET_KEY
 
 client = boto3.client(
-    's3'
-    aws_access_key_id=ACCESS_KEY
+    's3',
+    aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY
 )
