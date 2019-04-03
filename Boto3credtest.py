fdd0837f33ce9ed506696254383c43a91ada7bf8
pkartheiser86 <pkartheiser@gmail.com>
2019-04-02 20:47:38 -0600

    removed comma after secret_key pass

--- Boto3credtest.py
+++ Boto3credtest.py
@@ -5,8 +5,8 @@ from AWSSecrets import ACCESS_KEY, SECRET_KEY
 client = boto3.client(
     's3',
     aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
-)
+    aws_secret_access_key=SECRET_KEY
+    )
 
 
 for bucket in s3.buckets.all() :
