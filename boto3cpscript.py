dc92ca3c3dee995d1615d10f9593929ceca11030
pkartheiser86 <pkartheiser@gmail.com>
2019-04-03 21:42:43 -0600

    c

--- boto3cpscript.py
+++ boto3cpscript.py
@@ -7,7 +7,7 @@ s3 = boto3.client(
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_KEY
 )
-
+#prints list of keys inside specific named bucket
 def get_s3_keys(bucket):
     """Get a list of keys in an S3 bucket."""
     keys = []
@@ -16,3 +16,14 @@ def get_s3_keys(bucket):
         keys.append(obj['Key'])
     return keys
 print(get_s3_keys("pk-kapost-tech1"))
+
+def key_existing_size_list(client, bucket, key):
+    """return the key's size if it exist, else None"""
+    response = client.list_objects_v2(
+        Bucket=bucket,
+        Prefix=key,
+    )
+    for obj in response.get('Contents', []):
+        if obj['Key'] == key:
+            return obj['Size']
+print(key_existing_size_list(client, bucket, key))
