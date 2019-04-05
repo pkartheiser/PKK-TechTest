2d1d05248a134aeec5296bed6c4ccddfdd1a09d9
pkartheiser86 <pkartheiser@gmail.com>
2019-04-04 19:45:18 -0600

    example of aws secret key file.

---
+++ AWSKeyresource.py
@@ -0,0 +1,5 @@
+#AWS programmatic key resource, make sure your script references
+#this file name to load AWS credentials
+
+ACCESS_KEY='insert your access key here'
+SECRET_KEY='insert your secret key here'
